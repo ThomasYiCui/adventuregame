cd821f74701c3c0c865b6639fb6ab2c717d6a7e7
ThomasYiCui <93477247+ThomasYiCui@users.noreply.github.com>
2025-06-03 15:31:17 -0400

    Update minigunupgrade.js

--- minigunupgrade.js
+++ minigunupgrade.js
@@ -2,15 +2,15 @@ function minigunUpgrade() {
   if(player.spellInventory[1] === "minigun") {
     if(player.spellInventory[2] !== "minigun2") {
         button(20, 150, canvas.width/2 - 35, 50, [102, 51, 0], function() {
-            if(gems >= 7500) {
-                changeGems(-7500)
+            if(gems >= 2000) {
+                changeGems(-2000)
                 popUps.push(new popUp("Upgrade Sucsessful", canvas.width/2 - random(-canvas.width/3, canvas.width/3), canvas.height/2 + random(-canvas.height/3, canvas.height/3), 50, 300))
                 player.atkE = "minigun2";
                 player.spellInventory[2] = "minigun2"
                 player.reload = 5;
                 buy.play();
             }
-        }, ["Minigun Upgrade Lv. 2 [7500 Gems]", 20]);
+        }, ["Minigun Upgrade Lv. 2 [2000 Gems]", 20]);
     } else if(player.spellInventory[3] !== "minigun3") {
         button(20, 150, canvas.width/2 - 35, 50, [102, 51, 0], function() {
             if(gems >= 25000) {
@@ -24,15 +24,15 @@ function minigunUpgrade() {
         }, ["Minigun Upgrade Lv. 3 [25000 Gems]", 20]);
     } else if(player.spellInventory[4] !== "minigun4") {
         button(20, 150, canvas.width/2 - 35, 50, [102, 51, 0], function() {
-            if(gems >= 40000) {
-                changeGems(-40000)
+            if(gems >= 80000) {
+                changeGems(-80000)
                 popUps.push(new popUp("Upgrade Sucsessful", canvas.width/2 - random(-canvas.width/3, canvas.width/3), canvas.height/2 + random(-canvas.height/3, canvas.height/3), 50, 300))
                 player.atkE = "minigun4";
                 player.spellInventory[4] = "minigun4"
                 player.reload = 5;
                 buy.play();
             }
-        }, ["Minigun Upgrade Lv. 4 [40000 Gems]", 20]);
+        }, ["Minigun Upgrade Lv. 4 [80000 Gems]", 20]);
     } else if(player.spellInventory[4] === "minigun4") {
         button(20, 150, canvas.width/2 - 35, 50, [102, 51, 0], function() {
             popUps.push(new popUp("Switch Sucsessful", canvas.width/2 - random(-canvas.width/3, canvas.width/3), canvas.height/2 + random(-canvas.height/3, canvas.height/3), 50, 300))
@@ -43,14 +43,14 @@ function minigunUpgrade() {
     }
   } else {
       button(20, 150, canvas.width/2 - 35, 50, [102, 51, 0], function() {
-          if(gems >= 5000) {
-              changeGems(-5000)
+          if(gems >= 500) {
+              changeGems(-500)
               popUps.push(new popUp("Upgrade Sucsessful", canvas.width/2 - random(-canvas.width/3, canvas.width/3), canvas.height/2 + random(-canvas.height/3, canvas.height/3), 50, 300))
               player.atkE = "minigun";
               player.spellInventory[1] = "minigun"
               player.reload = 5;
               buy.play();
           }
-      }, ["Minigun Upgrade Lv. 1 [5000 Gems]", 20]);
+      }, ["Minigun Upgrade Lv. 1 [500 Gems]", 20]);
   }
 }
